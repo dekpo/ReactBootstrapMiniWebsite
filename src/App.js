b8e0d7472ed4f3815d7cbb2b7348a8b2d3006f97
Dekpo <dekpo@me.com>
2021-09-30 10:21:57 +0200

    Added a navbar logo and style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="">
-        <h1>Header</h1>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <img src={require('./assets/logo-m2i.jpeg').default} alt="logo M2i" width="50" />
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
       </header>
       <section>
         <div className="container-fluid">
@@ -16,10 +37,10 @@ function App() {
           <div className="row">
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
-        </div> 
+        </div>
       </section>
-      <footer>
-        <h2>Footer</h2>
+      <footer className="mt-3 bg-dark text-white">
+       <p>&copy; M2i - CDA</p>
       </footer>
     </div>
   );
