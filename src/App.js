858d4e15e9588d67de56d1d1e38f9ea40fc34b9b
Dekpo <dekpo@me.com>
2021-09-30 11:00:53 +0200

    Created few components ...

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="">
-
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              <img src={require('./assets/logo-m2i.jpeg').default} alt="logo M2i" width="50" />
-            </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-      </header>
-      <section>
-        <div className="container-fluid">
-          <div className="row">
-            <h3>Home</h3>
-          </div>
-          <div className="row">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-        </div>
-      </section>
-      <footer className="mt-3 bg-dark text-white">
-       <p>&copy; M2i - CDA</p>
-      </footer>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <footer className="mt-3 p-1 bg-dark text-white text-center">
+    <p>&copy; M2i - CDA</p>
+   </footer>
+)
+
+export default Footer
